0ea351b91b63488942e53fb1ca69e01e8d116842
zoohee <jjhh1545@gmail.com>
2023-12-10 23:07:40 +0900

    Section 4 : HATEOAS for Level3

--- src/main/java/com/zoohee/myrestfulservice/controller/UserController.java
+++ src/main/java/com/zoohee/myrestfulservice/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.zoohee.myrestfulservice.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import com.zoohee.myrestfulservice.bean.User;
 import com.zoohee.myrestfulservice.dao.UserDaoService;
 import com.zoohee.myrestfulservice.exception.UserNotFoundException;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import javax.swing.text.html.parser.Entity;
 import org.apache.coyote.Response;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,14 +36,19 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveAllUsers(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        EntityModel entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        // retrieveAllUsers()가 /users 로 매핑되어 있기 때문에 여기로 참조됨
+        entityModel.add(linTo.withRel("all-users")); // all-users -> http://localhost:8088/users
+
+        return entityModel;
     }
 
     @PostMapping("/users")
